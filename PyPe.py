b9e109d15c7ece8158e124cb301a679f3aaee6ec
spambi <spambayyyy@gmail.com>
2019-04-29 20:46:56 +1000

    threading was problem, working now, still need to fix threading

--- PyPe.py
+++ PyPe.py
@@ -4,31 +4,31 @@ import threading
 import time
 
 
-
-
 class GUI(wx.Frame):
 
 	mainPanel 	= None
 	vbox		= None
 	hbox1		= None
-	hbox2		= None
+	hbox2 		= None	
 	msgHistory	= None
 	msgWindow	= None
+	sendButton	= None
 
-	def __init__(self, threadname, *args, **kwargs):
+	def __init__(self, *args, **kwargs):
 		super(GUI, self).__init__(*args, **kwargs)
 
-		self.t = threading.Thread(target = self.ui_test, name = threadname)
-		self.t.start()
+		#self.t = threading.Thread(target = self.ui_test, name = threadname)
+		#self.t.start()
 
 		self.ui_test()
 
 		self.SetSize(1280, 640)
 		self.Center()
 
+
 	def ui_test(self):
 
-		self.mainPanel = wx.Panel(self, wx.NewIdRef())
+		self.mainPanel = wx.Panel(self)
 
 		self.vbox = wx.BoxSizer(wx.VERTICAL)
 		# Horizontal Boxes, set name later
@@ -39,31 +39,38 @@ class GUI(wx.Frame):
 									wx.TE_AUTO_URL)
 		self.msgWindow = wx.TextCtrl(self.mainPanel, style=wx.TE_MULTILINE | wx.TE_AUTO_URL )
 		# Buttons etc,
-		#sendButton = wx.Button(self.mainPanel, label="Send", size(50, 39))
+		self.sendButton = wx.Button(self.mainPanel, label="Send", size=(50, 39))
+		
 
+		self.hbox1.Add(self.msgHistory, proportion=1, flag=wx.EXPAND)
+		self.hbox2.Add(self.msgWindow, proportion=1, flag=wx.EXPAND)
+		self.hbox2.Add(self.sendButton, flag= wx.EXPAND | wx.RIGHT, border=10)
+		self.vbox.Add(self.hbox1, proportion=1, flag=wx.LEFT | wx.RIGHT | wx.TOP | wx.EXPAND, border=10)
+						
+		self.vbox.Add(self.hbox2, flag=wx.EXPAND | wx.LEFT | wx.RIGHT | wx.TOP,
+                 border=10)
 
+		self.msgWindow.AppendText('im a weeb lol, www.magicalgirls.moe/dance')
 
-
-		self.hbox1.Add(self.msgHistory, proportions=1, flag=wx.EXPAND)
-		self.msgHistory.AppendText('ahahaha')
-		self.vbox.Add(self.hbox1, flag=wx.LEFT | wx.RIGHT | 
-						wx.EXPAND, border=10)
+		#self.Bind(wx.EVT_BUTTON, self.msgGUI, self.sendButton)
+		self.Bind(wx.EVT_BUTTON, lambda evt: self.msgGUI(), self.sendButton)
 
 		self.vbox.Add((-1, 10))
 
-		self.SetSizer(self.vbox)
+		self.mainPanel.SetSizer(self.vbox)
 
+	def msgGUI(self):
+		msg = self.msgWindow.GetValue()
+		self.msgHistory.AppendText('{}{}\n'.format(usernamemsg))
+		self.msgWindow.Clear()
+		print msg
 
 
 if __name__ == '__main__':
 	
-	
     APP = wx.App()
-    GUI1 = GUI('gui_test', None, title='testing')
+    GUI1 = GUI(None, title='testing')
     GUI1.Show()
-
-    #time.sleep(2)
-    #GUI1.msgWindow.AppendText('hello')
 
     APP.MainLoop()
     
